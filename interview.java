819c33462db6ba715e720c5d3f59053f1d3d5be3
kanakip <pallavikanaki13@gmail.com>
2025-05-20 12:46:39 +0530

    Create interview.java

---
+++ interview.java
@@ -0,0 +1,27 @@
+𝗣𝗛𝗔𝗦𝗘 𝟭 - 𝗕𝗮𝘀𝗶𝗰𝘀
+
+• What is Apache Kafka and what are its main use cases?
+• Explain the key components of Kafka: Producer, Consumer, Broker, Topic, Partition.
+• What is a Kafka topic and how does partitioning work in Kafka?
+• Difference between Kafka and traditional message queues?
+• What is a consumer group in Kafka, and how does it help in load balancing?
+
+𝗣𝗛𝗔𝗦𝗘 𝟮 - 𝗜𝗻𝘁𝗲𝗿𝗺𝗲𝗱𝗶𝗮𝘁𝗲
+
+• Explain how Kafka ensures message durability and fault tolerance.
+• What is ISR (In-Sync Replicas) in Kafka?
+• How does Kafka handle message ordering and delivery semantics (at-least-once, exactly-once, at-most-once)?
+• What is the role of Kafka Zookeeper?
+• Explain Kafka producer acknowledgments (acks=0, acks=1, acks=all).
+
+𝗣𝗛𝗔𝗦𝗘 𝟯 - 𝗔𝗱𝘃𝗮𝗻𝗰𝗲𝗱
+
+• Explain Kafka’s exactly-once semantics and how it is implemented.
+• What is Kafka log compaction and when would you use it?
+• Difference between Kafka Streams and Kafka Connect.
+• How do you tune Kafka performance for high-throughput systems?
+• Explain the end-to-end flow of data in a real-time streaming pipeline using Kafka integrated with other GCP services like Dataflow or BigQuery.
+
+From <https://www.linkedin.com/search/results/all/?keywords=kafka%20interview&origin=TYPEAHEAD_HISTORY&page=2&searchId=8a71c2e6-43f0-4f28-98a3-f55af85e0ea4&sid=pVa&spellCorrectionEnabled=true> 
+
+  https://www.interviewbit.com/kafka-interview-questions/
